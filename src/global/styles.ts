15114cf94b07450cff0370312347acbca6ed5e8a
José Carlos Noronha <joseph.finder33@gmail.com>
2022-09-01 23:05:24 -0300

    :zap: Add Pagination Into Main Page

--- src/global/styles.ts
+++ src/global/styles.ts
@@ -1,4 +1,8 @@
 import styled from "styled-components/native";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { Dimensions } from "react-native";
+
+const screenWidth = Dimensions.get('window').width;
 
 export const Container = styled.View`
   flex: 1;
@@ -10,5 +14,10 @@ export const AnimatedComponentBox = styled.View`
   flex: 1;
   align-items: center;
   justify-content: space-evenly;
+`;
+
+export const Box = styled.View`
+  align-items: center;
+  justify-content: center;
 `;
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
-import AnimatedMenuVertically from './components/AnimatedMenuVertically';
-import { Container, AnimatedComponentBox } from './global/styles';
+import { Animated, Dimensions, View } from 'react-native';
+import AnimatedMenuVertically from './components/AnimatedMenus/AnimatedMenuVertically';
+import { Container, AnimatedComponentBox, Box } from './global/styles';
 import GradientText from './components/GradientText';
 
 // import { Container } from './styles';
 
 const MicroAnimations: React.FC = () => {
+  const { width, height } = Dimensions.get('window');
+
   return (
     <Container>
       <GradientText
@@ -17,29 +19,50 @@ const MicroAnimations: React.FC = () => {
           { color: "#2b1ac8", percent: "70%" },
         ]}
       />
-      <AnimatedComponentBox>
-        <AnimatedMenuVertically
-          height={20}
-          width={20}
-          borderRadius={15}
-          activeOpacity={0.6}
-          backgroundColor={"#f5f5f5"}
-        />
-        <AnimatedMenuVertically
-          height={30}
-          width={30}
-          borderRadius={20}
-          activeOpacity={0.6}
-          backgroundColor={"#f5f5f5"}
-        />
-        <AnimatedMenuVertically
-          height={50}
-          width={50}
-          borderRadius={40}
-          activeOpacity={0.6}
-          backgroundColor={"#f5f5f5"}
-        />
-      </AnimatedComponentBox>
+      <Animated.FlatList
+        data={[1234, 2345]}
+        keyExtractor={item => item.toString()}
+        renderItem={({ item, index }) => {
+          return (
+            <Box key={item} style={{ width: width - 13, height: "100%" }}>
+              {
+                index == 0 ? (
+                  <AnimatedComponentBox>
+                    <AnimatedMenuVertically
+                      height={20}
+                      width={20}
+                      borderRadius={15}
+                      activeOpacity={0.6}
+                      backgroundColor={"#f5f5f5"}
+                    />
+                    <AnimatedMenuVertically
+                      height={30}
+                      width={30}
+                      borderRadius={20}
+                      activeOpacity={0.6}
+                      backgroundColor={"#f5f5f5"}
+                    />
+                    <AnimatedMenuVertically
+                      height={50}
+                      width={50}
+                      borderRadius={40}
+                      activeOpacity={0.6}
+                      backgroundColor={"#f5f5f5"}
+                    />
+                  </AnimatedComponentBox>
+                ) : (
+                  <>
+                  </>
+                )
+              }
+            </Box>
+          )
+        }}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        style={{ padding: 0 }}
+      />
     </Container>
   );
 }
